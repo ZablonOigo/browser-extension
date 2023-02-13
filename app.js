d1968b1f577c91a71a179e40fcba6c649d0646e9
Zablon Oigo <imjunior254@gmail.com>
2023-02-13 08:56:41 +0300

    chrome extension

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-const myarr=[];
-const link=document.querySelector('#link');
-const input=document.querySelector('#input-el');
-const  button=document.querySelector('#save-btn').addEventListener('click',()=>{
-console.log('button clicked');
-myarr.push(input.value);
-input.value=' ';
-renderLeads()
-}) ;
-function renderLeads(){
-    for(let i in myarr){
-        link.innerHTML+=`<li>${myarr[i]}</li>`;
-    }
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+const tabBtn=document.querySelector('#tab-btn');
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
+tabBtn.addEventListener('click',()=>{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads);
+
+    })
+
+});
+
+
+function render(leads) {
+    for (let i = 0; i < leads.length; i++) {
+        ulEl.innerHTML += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+  
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
